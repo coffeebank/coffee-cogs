c32b459d3dcb344d9dde652388986e07777fd47a
Nightscape Coffee <0c48d607@opayq.com>
2021-02-04 21:24:20 -0800

    Dependencies

--- loveping/__init__.py
+++ loveping/__init__.py
@@ -1,5 +1,9 @@
+import json
+from pathlib import Path
+from redbot.core.bot import Red
 from .loveping import loveping
 
-
-def setup(bot):
-    bot.add_cog(loveping())
+async def setup(bot: Red) -> None:
+    love_ping = loveping(bot)
+    bot.add_cog(love_ping)
+    await love_ping.initialize()
