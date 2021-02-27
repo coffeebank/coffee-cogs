a8605d60678b4d974547834618b499321e654c39
Nightscape Coffee <0c48d607@opayq.com>
2021-02-27 04:10:09 -0800

    Update sendhook

--- sendhook/sendhook.py
+++ sendhook/sendhook.py
@@ -12,33 +12,104 @@ class sendhook(commands.Cog):
     def __init__(self):
         self.config = Config.get_conf(self, identifier=806715409318936616)
         default_guild = {
-            "webhooks": ""
+            "webhookAlias": {}
         }
         self.config.register_guild(**default_guild)
 
-    '''
+
     @commands.command()
-    @checks.admin_or_permissions(manage_guild=True)
-    async def sendhook(self, ctx, message):
-        """Set webhooks"""
-        await self.config.guild(ctx.guild).webhooks.set(message)
-        await ctx.send("")
-    '''
+    async def aliashook(self, ctx, subcommand, alias_name="", webhookUrl=""):
+        """Aliases for your webhooks
+        
+        [p]aliashook list
+        [p]aliashook add <alias_name> <webhookUrl>
+        [p]aliashook remove <alias_name>
+        """
+        webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
+        if subcommand == "add":
+            webhookAlias[alias_name] = webhookUrl
+            await self.config.guild(ctx.guild).webhookAlias.set(webhookAlias)
+            # Try adding react, if no perms then send normal message
+            try:
+                await ctx.message.add_reaction("✅")
+            except:
+                await ctx.send("Webhook alias added ✅")
+        elif subcommand == "remove":
+            try:
+                # Try to set alias to empty, before removing
+                webhookAlias[alias_name] = webhookUrl
+                del webhookAlias[alias_name]
+                await self.config.guild(ctx.guild).webhookAlias.set(webhookAlias)
+                # Try adding react, if no perms then send normal message
+                try:
+                    await ctx.message.add_reaction("✅")
+                except:
+                    await ctx.send("Webhook alias removed ✅")
+            except KeyError:
+                pass
+        else:
+            await ctx.send(webhookAlias)
+
+
+    # @commands.bot_has_permissions(embed_links=True, add_reactions=True)
 
     @commands.command()
     async def sendhook(self, ctx, webhookUrl, *, webhookText):
         """Send a webhook"""
 
-        hook = Webhook(webhookUrl)
-        hook.send(webhookText)
-        await ctx.send("Sent webhook")
+        # Check if webhookUrl is an alias
+        webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
+        if webhookUrl in webhookAlias:
+            hook = Webhook(webhookAlias[webhookUrl])
+        else:
+            hook = Webhook(webhookUrl)
+        
+        try:
+            hook.send(webhookText)
+        except:
+            await ctx.send("Oh no! Webhook couldn't be sent :(")
+        else:
+            # Try adding react, if no perms then send normal message
+            try:
+                await ctx.message.add_reaction("✅")
+            except:
+                await ctx.send("Webhook sent ✅")
+
 
     @commands.command()
     async def edithook(self, ctx, webhookUrl, messageId, *, webhookText):
         """Edit a webhook"""
-        head = {"Content-Type":"application/json"}
-        url = webhookUrl + "/messages/" + str(messageId)
-        payload = {"content": webhookText }
 
-        requests.patch(url, payload, headers=head)
-        await ctx.send("Updated webhook")
+        # Formatting the messageId
+        forgotMsgLink = "Oh no! Did you remember to include the message link to the message you want to edit?"
+        if isinstance(messageId, str):
+            try:
+                messageId = int(messageId.split('/')[-1])
+            except:
+                await ctx.send(forgotMsgLink)
+        elif isinstance(messageId, int):
+            pass
+        else:
+            await ctx.send(forgotMsgLink)
+
+        # Check if webhookUrl is an alias
+        webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
+        if webhookUrl in webhookAlias:
+            url = str(webhookAlias[webhookUrl]) + "/messages/" + str(messageId)
+        else:
+            url = str(webhookUrl) + "/messages/" + str(messageId)
+
+        # Formatting the payload
+        head = {"Content-Type":"application/json"}
+        payload = {"content": str(webhookText) }
+
+        try:
+            requests.patch(url, json=payload, headers=head)
+        except:
+            await ctx.send("Oh no! Webhook couldn't be sent :(")
+        finally:
+            # Try adding react, if no perms then send normal message
+            try:
+                await ctx.message.add_reaction("✅")
+            except:
+                await ctx.send("Webhook updated ✅")
