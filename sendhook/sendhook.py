aa9d7aff74ceb2230a9654bd53dc953261cf18dd
Nightscape Coffee <0c48d607@opayq.com>
2021-03-10 16:24:38 -0800

    sendhook: update to .group(), improve aliashook list

--- sendhook/sendhook.py
+++ sendhook/sendhook.py
@@ -1,6 +1,5 @@
-# from redbot.core import Config
 from redbot.core import Config, commands, checks
-from array import *
+# from array import *
 from dhooks import Webhook, Embed
 import asyncio
 import requests
@@ -17,45 +16,65 @@ class sendhook(commands.Cog):
         self.config.register_guild(**default_guild)
 
 
-    @commands.command()
-    async def aliashook(self, ctx, subcommand, alias_name="", webhookUrl=""):
-        """Aliases for your webhooks
-        
-        [p]aliashook list
-        [p]aliashook add <alias_name> <webhookUrl>
-        [p]aliashook remove <alias_name>
-        """
+    @commands.guild_only()
+    @commands.group()
+    @checks.mod()
+    async def aliashook(self, ctx: commands.Context):
+        """Configure aliases for webhooks in your server"""
+        if not ctx.invoked_subcommand:
+            pass
+
+    @aliashook.command(name="add")
+    async def ahadd(self, ctx, alias, webhookUrl):
+        """Add an alias for a webhook"""
         webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
-        if subcommand == "add":
-            webhookAlias[alias_name] = webhookUrl
+        webhookAlias[alias] = webhookUrl
+        await self.config.guild(ctx.guild).webhookAlias.set(webhookAlias)
+        # Try adding react, if no perms then send normal message
+        try:
+            await ctx.message.add_reaction("✅")
+        except:
+            await ctx.send("Webhook alias added ✅")
+
+    @aliashook.command(name="remove")
+    async def ahremove(self, ctx, alias):
+        """Remove an alias for a webhook"""
+        webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
+        try:
+            # Try to set alias to empty, before removing
+            webhookAlias[alias] = ""
+            del webhookAlias[alias]
             await self.config.guild(ctx.guild).webhookAlias.set(webhookAlias)
             # Try adding react, if no perms then send normal message
             try:
                 await ctx.message.add_reaction("✅")
             except:
-                await ctx.send("Webhook alias added ✅")
-        elif subcommand == "remove":
-            try:
-                # Try to set alias to empty, before removing
-                webhookAlias[alias_name] = webhookUrl
-                del webhookAlias[alias_name]
-                await self.config.guild(ctx.guild).webhookAlias.set(webhookAlias)
-                # Try adding react, if no perms then send normal message
-                try:
-                    await ctx.message.add_reaction("✅")
-                except:
-                    await ctx.send("Webhook alias removed ✅")
-            except KeyError:
-                pass
-        else:
-            await ctx.send(webhookAlias)
+                await ctx.send("Webhook alias removed ✅")
+        except KeyError:
+            pass
+
+    @aliashook.command(name="list")
+    async def ahlist(self, ctx):
+        """List all aliases for webhooks"""
+        webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
+        webhookData = json.dumps(webhookAlias, sort_keys=True, indent=2, separators=(',', ': '))
+        await ctx.send("```json\n"+webhookData+"```")
+
+    @aliashook.command(name="show")
+    async def ahshow(self, ctx, alias):
+        """Show the saved webhook url for an alias"""
+        webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
+        webhookData = webhookAlias[alias]
+        await ctx.send("```"+webhookData+"```")
 
 
     # @commands.bot_has_permissions(embed_links=True, add_reactions=True)
 
     @commands.command()
     async def sendhook(self, ctx, webhookUrl, *, webhookText):
-        """Send a webhook"""
+        """Send a webhook
+        
+        webhookUrl can be an alias"""
 
         # Check if webhookUrl is an alias
         webhookAlias = await self.config.guild(ctx.guild).webhookAlias()
@@ -78,7 +97,9 @@ class sendhook(commands.Cog):
 
     @commands.command()
     async def edithook(self, ctx, webhookUrl, messageId, *, webhookText):
-        """Edit a webhook"""
+        """Edit a webhook
+        
+        webhookUrl can be an alias"""
 
         # Formatting the messageId
         forgotMsgLink = "Oh no! Did you remember to include the message link to the message you want to edit?"
