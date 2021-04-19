147783cd0622991a2267e60f00df947b6fb1f917
Nightscape Coffee <0c48d607@opayq.com>
2021-04-19 16:54:30 -0700

    coffeetools: Add choose command

---
+++ coffeetools/__init__.py
@@ -0,0 +1,4 @@
+from .coffeetools import Coffeetools
+
+def setup(bot):
+    bot.add_cog(Coffeetools())
---
+++ coffeetools/coffeetools.py
@@ -0,0 +1,28 @@
+from redbot.core import Config, commands, checks
+import random
+
+class Coffeetools(commands.Cog):
+    """Gives you links to common search engines based on a search query."""
+
+    def __init__(self):
+        self.config = Config.get_conf(self, identifier=806715409318936616)
+
+    async def red_get_data_for_user(self, *, user_id: int):
+        # this cog does not story any data
+        return {}
+
+    async def red_delete_data_for_user(self, *, requester, user_id: int) -> None:
+        # this cog does not story any data
+        pass
+
+    @commands.command()
+    async def choose(self, ctx, *, choosetext):
+        """Have the bot choose for you
+        
+        **`[p]choose item1 | item2 | item3`**"""
+
+        # Split choosetext into an array
+        choosearray = choosetext.split("|")
+
+        # Return a random choice from array
+        return await ctx.send(random.choice(choosearray))
