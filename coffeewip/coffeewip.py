036a7e13694ca72507f91c256cf65f2dd1a2dbd9
Nightscape Coffee <coffeebank@catdev.anonaddy.com>
2021-05-19 13:37:33 -0700

    coffeewip: Add embedtest

--- coffeewip/coffeewip.py
+++ coffeewip/coffeewip.py
@@ -23,3 +23,12 @@ class Coffeewip(commands.Cog):
     async def red_delete_data_for_user(self, *, requester, user_id: int) -> None:
         # this cog does not story any data
         pass
+
+        
+    @commands.command()
+    async def embedtest123123(self, ctx):
+        """embedtest123123"""
+        botcolor = await ctx.embed_colour()
+        e = discord.Embed(color=botcolor, description="desc")
+        e.set_image(url="https://images-ext-2.discordapp.net/external/cC-YBJkH2GXnX7MHMASUM9Gle1S1im3rDJj2K54A28w/%3Fcid%3D73b8f7b19a5ccc575679c0a7fc4a673b753e4ce993f35223%26rid%3Dgiphy.mp4/https/media2.giphy.com/media/Q8bEDnj9hZd6vivXSZ/giphy.mp4")
+        await ctx.send(embed=e)
