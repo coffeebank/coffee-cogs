3996d30205904a88b9dba80101e54de20f1eb02d
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-03-05 02:31:59 -0800

    wordledb: Fix line breaks

--- wordledb/wordledb.py
+++ wordledb/wordledb.py
@@ -36,7 +36,7 @@ class Wordledb(commands.Cog):
         client = pymongo.MongoClient(mongoUrl, serverSelectionTimeoutMS=5000)
         db = client[dbName]
         coll = db[collectionName]
-        post = coll.insert_one({"date": date, "wordle": wordle, "answer": answer[2:-2] })
+        post = coll.insert_one({"date": date, "wordle": repr(wordle)[1:-1], "answer": answer[2:-2] })
         try:
           await ctx.send("✅\n"+str(post.inserted_id)[:1984])
         except Exception as e:
