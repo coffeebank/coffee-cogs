e1eb5b47658007654c718fedbab3023d2f31ca45
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2023-01-15 00:03:44 -0800

    Initial commit

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+// custom typefaces
+import "@fontsource/montserrat/variable.css"
+import "@fontsource/merriweather"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
