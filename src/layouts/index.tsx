738ca68dc4469e4d91efbb182b5a9eb8c8948bad
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2023-02-22 01:59:43 -0800

    feat: add catsjs

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { useEffect } from "react"
+import { useLocation } from "@reach/router"
 import Sidebar from "../components/Sidebar"
 
 interface Props {
@@ -6,6 +8,69 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
+  const location = useLocation()
+
+  useEffect(() => {
+    // Client only
+    try { process } catch (e) {
+      if (JSON.parse(localStorage.getItem("catsIgnore")) !== "true" && window.location.hostname !== "localhost") {
+        console.log('🐱 loaded '+location.pathname);
+        
+        // collect variables
+        let catsSession;
+        let catsTime;
+        let dateObj;
+        let catsUserAgent;
+        let catsPage;
+  
+        try {
+          // use iso timestamp as session id
+          if (localStorage.getItem("catsSession")) {
+            // ongoing session
+            catsSession = localStorage.getItem("catsSession");
+            dateObj = new Date();
+            catsTime = dateObj.toString();
+          } else {
+            // new session
+            dateObj = new Date();
+            catsSession = Date.parse(dateObj);
+            catsTime = dateObj.toString();
+            localStorage.setItem("catsSession", catsSession);
+          }
+          console.log("catsjs: session timestamp " + catsSession);
+  
+          catsUserAgent = navigator.userAgent;
+          catsPage = location.pathname;
+        } catch (error) {
+          console.log({ error }, "catsjs: variable collect error");
+        }
+
+        // send to database
+        try {
+          const catsPostOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify({
+              sessionId: catsSession,
+              time: catsTime,
+              useragent: catsUserAgent,
+              page: catsPage,
+            })
+          };
+          fetch("https://coffeebank.netlify.app/.netlify/functions/postCats", catsPostOptions)
+            .then(catsPostResp => console.log("catsjs: updated " + catsPage))
+            .catch((error) => {
+              console.log(error)
+            });
+        } catch (error) {
+          console.log({ error }, "catsjs: couldn't connect")
+        }
+      } else {
+        console.log("catsjs: excluded device");
+      }
+    }
+  }, [location.pathname])
+
   return (
     <div className="dark:bg-[#111111] min-h-screen">
       <div className="max-w-[1400px] mx-auto md:pl-2 md:pr-6 lg:px-8">
