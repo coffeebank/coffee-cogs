96d0d049c424e28c70c9ea9c8aed10d8db0a793c
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2023-05-21 22:00:48 -0700

    fix(hellohook): update init

--- hellohook/__init__.py
+++ hellohook/__init__.py
@@ -3,5 +3,5 @@ from redbot.core.utils import get_end_user_data_statement
 
 __red_end_user_data_statement__  = get_end_user_data_statement(__file__)
 
-def setup(bot):
-    bot.add_cog(Hellohook(bot))
+async def setup(bot):
+    await bot.add_cog(Hellohook(bot))
